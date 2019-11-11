ac1c5bf68575632193c2b5396f936c12792888a0
ShimonOcean <56456733+ShimonIslam@users.noreply.github.com>
2019-11-11 14:48:47 -0800

    Added Bounding Box for Single Face

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ const app = new Clarifai.App({
   apiKey: 'a5e8e4db3f9c430ca66fbf19886ecf20'
 });
 
+// Background Particle effects options
 const particlesSelect = {
   particles: {
     number: {
@@ -25,35 +26,56 @@ const particlesSelect = {
   }
 }
 
+// Holds state for inputs, images, bounding box for faces
 class App extends Component {
   constructor() {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {},
     }
   }
 
+  // Calculates dimensions for bounding box
+  calcFaceLocation = (data) => {
+    const boxSize = data.outputs[0].data.regions[0].region_info.bounding_box
+    const image = document.getElementById('inputImage')
+    const width = Number(image.width)
+    const height = Number(image.height) 
+    const newArr = data.outputs[0].data.regions.map(region => {
+        return{
+            leftCol:region.region_info.bounding_box.left_col * width,
+            topRow: region.region_info.bounding_box.top_row * height,
+            rightCol:width - (region.region_info.bounding_box.right_col * width),
+            bottomRow: height - (region.region_info.bounding_box.bottom_row * height)
+        } 
+    })
+    return newArr
+  }
+
+  // Sets state of box to calculated box
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
+
+  // On input change in URL input, updates input state
   onInputChange = (event) => {
     this.setState({input: event.target.value});
   }
 
+  // On submit, sets imageUrl state to submitted link
   onSubmit = () => {
     this.setState({imageUrl: this.state.input});
     app.models.predict(
       Clarifai.FACE_DETECT_MODEL, 
-      "https://samples.clarifai.com/metro-north.jpg")
-      .then(
-      function(response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      },
-      function(err) {
-  
-      }
-    );
-
+      this.state.input)
+      // Sets state of box to calculated box
+      .then(response => this.displayFaceBox(this.calcFaceLocation(response)))
+      .catch(err => console.log(err));
   }
 
+  // Components to render to website
   render() {
     return (
       <div className="App">
@@ -66,7 +88,7 @@ class App extends Component {
         <ImgLinkForm 
           onInputChange={this.onInputChange} 
           onSubmit={this.onSubmit}/>
-        <FaceRecog imageUrl={this.state.imageUrl}/>
+        <FaceRecog box={this.state.box} imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
